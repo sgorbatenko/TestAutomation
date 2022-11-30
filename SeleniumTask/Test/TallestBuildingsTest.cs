54a48d79f338d07f677e9659ad58a2ac2a2cb261
Stanislav <sgorbatenko8@gmail.com>
2022-11-30 16:18:43 +0100

    small cleanup

--- SeleniumTask/Test/TallestBuildingsTest.cs
+++ SeleniumTask/Test/TallestBuildingsTest.cs
@@ -33,7 +33,7 @@ namespace SeleniumTask.Test
 
             tbPage.SelectFromDropdown("100 Tallest Completed Buildings in the World");                   
 
-            Assert.That(tbPage.GetRowByText("Lotte World Tower").FindElements(By.TagName("td"))[6].Text, Is.EqualTo("123"));
+            Assert.That(tbPage.GetRowByText("Lotte World Tower").FindElements(By.TagName("td"))[(int)Header.Floors].Text, Is.EqualTo("123"));
 
             Assert.That(tbPage.GetNumberOfRecords(), Is.EqualTo(100));
 
